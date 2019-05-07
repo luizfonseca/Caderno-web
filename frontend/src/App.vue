e03ea407261dfd8f0d56a163e71abf1c3bbf6058
Luiz Fonseca <eu@luiz.cc>
2019-05-07 23:19:33 +0200

    First commit with the Editor initial setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Editor/>
+  </div>
+</template>
+
+<script>
+import Editor from "./components/Editor.vue";
+import "./assets/css/main.css";
+
+export default {
+  name: "app",
+  components: {
+    Editor
+  }
+};
+</script>
---
+++ go.mod
@@ -0,0 +1,6 @@
+module caderno
+
+require (
+	github.com/leaanthony/mewn v0.10.5
+	github.com/wailsapp/wails v0.12.3
+)
